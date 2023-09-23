d5b2bc26ec4242afe08fa5750016a8430ab39f8d
JuanMartinUribe <jmuribef@eafit.edu.co>
2023-09-22 21:22:31 -0500

    delete unused methods and change unit test status code

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,11 @@ async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!!!")
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
     })
     .bind(("0.0.0.0", 3000))?
     .run()
@@ -35,7 +25,7 @@ mod tests {
     async fn test_init_service() {
         let app = test::init_service(
             App::new()
-                .service(web::resource("/test").to(|| async { "OK" }))
+                .service(web::resource("/").to(|| async { "OK" }))
         ).await;
     
         // Create request object
@@ -43,7 +33,7 @@ mod tests {
     
         // Execute application
         let res = app.call(req).await.unwrap();
-        assert_eq!(res.status(), StatusCode::OK);
+        assert_eq!(res.status(), StatusCode::NOT_FOUND);
     }
 
 }
